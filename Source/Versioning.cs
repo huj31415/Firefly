5bfb5ba5a329d4b00c6ff1f62b823c92ff9f4dab
MirageDev <forpixel.studio@gmail.com>
2025-01-22 22:26:21 +0100

    rebuild for non-dev

--- Source/Versioning.cs
+++ Source/Versioning.cs
@@ -4,6 +4,6 @@
 	{
 		public static string VersionAuthor = "MirageDev,thunderchild,Knedlik,JonnyOThan";
 		public static string Version = "b4";
-		public static bool IsDev = true;
+		public static bool IsDev = false;
 	}
 }

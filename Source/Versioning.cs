0a547cdeb5bf8ab6f1df552631931c1a5630baf3
MirageDev <forpixel.studio@gmail.com>
2025-02-01 20:51:34 +0100

    s

--- Source/Versioning.cs
+++ Source/Versioning.cs
@@ -3,7 +3,6 @@
 	public class Versioning
 	{
 		public static string VersionAuthor = "MirageDev,thunderchild,Knedlik,JonnyOThan";
-		public static string Version = "b4";
-		public static bool IsDev = false;
+		public static string Version = "b2";
 	}
 }
